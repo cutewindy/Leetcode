5c649850f7e9c48b5beb370e46d816dd6beae103
cutewindy <wengwendi@gmail.com>
2015-05-06 16:18:13 +0800

    First commit

---
+++ prime in 100.py
@@ -0,0 +1,30 @@
+def prime(n):
+    if n <= 0:
+        return
+
+    prime = []
+    if n == 1:
+        prime = []
+    elif n == 2:
+        prime = [2]
+    else:
+        prime = [2]
+        for i in range(3, 100):
+            j = 2
+            while j < i:
+                
+                if i % j == 0:
+                    break
+                j += 1
+            else:   
+                prime.append(i)
+                
+    return prime
+
+
+print prime(1)
+print prime(2)
+print prime(100)
+print prime(-8)
+
+print len(prime(100))
