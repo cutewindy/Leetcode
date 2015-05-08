6f512c01056240acb7816879f39814b6f9738893
cutewindy <wengwendi@gmail.com>
2015-05-08 11:18:29 +0800

    First solution

--- Best_Time_to_Buy_and_Sell_Stock_II.py
+++ Best_Time_to_Buy_and_Sell_Stock_II.py
@@ -3,3 +3,35 @@
 # Design an algorithm to find the maximum profit. You may complete as many transactions as you like 
 # (ie, buy one and sell one share of the stock multiple times). However, you may not engage in multiple 
 # transactions at the same time (ie, you must sell the stock before you buy again).
+
+
+def maxProfit(prices):
+    earn = 0
+#     if len(prices) == 0 or len(prices) == 1:
+#         return earn
+#     elif len(prices) == 2 and prices[0] <= prices[1]:
+#         return prices[1] - prices[0]
+#     elif len(prices) > 2:
+#         if prices[0] <= prices[1]:
+#             earn -= prices[0]
+#         for i in range(1, len(prices) - 1):
+#             if prices[i - 1] > prices[i] and prices[i] <= prices[i + 1]:
+#                 earn -= prices[i] 
+#             if prices[i - 1] < prices[i] and prices[i] >= prices[i + 1]:
+#                 earn += prices[i]  
+#         if prices[len(prices) - 2] < prices[len(prices) - 1]:
+#             earn += prices[len(prices) - 1]
+
+    for i in range(len(prices)-1):
+        if prices[i] < prices[i + 1]:
+            earn += prices[i+1] - prices[i]
+    return earn
+
+     
+print maxProfit([2, 1, 3, 4, 5, 2, 1])
+print maxProfit([2, 1, 3, 4, 5, 1, 2])
+print maxProfit([])
+print maxProfit([2, 2, 5])
+print maxProfit([3, 3])
+print maxProfit([2, 2])
+
--- Search_Insert_Position.py
+++ Search_Insert_Position.py
@@ -3,22 +3,19 @@
 # You may assume no duplicates in the array.
 
 # Here are few examples.
-# [1,3,5,6], 5 → 2
-# [1,3,5,6], 2 → 1
-# [1,3,5,6], 7 → 4
-# [1,3,5,6], 0 → 0
+# [1,3,5,6], 5  2
+# [1,3,5,6], 2  1
+# [1,3,5,6], 7  4
+# [1,3,5,6], 0  0
 
 
 
 def searchInsert(nums, target):
-    count = 0
     for num in nums:
         if num >= target:
             return nums.index(num)
-        else:
-            count += 1
-    if count == len(nums):
-        return len(nums)
+
+    return len(nums)
 
 
 
@@ -26,3 +23,7 @@ def searchInsert(nums, target):
 print searchInsert([1, 3, 5, 6], 5)
 print searchInsert([1, 3, 5, 6], 2)
 print searchInsert([1], 2)
+
+
+
+
