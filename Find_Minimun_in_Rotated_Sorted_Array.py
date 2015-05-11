de806c09034dbb5bc966212bc405a165f1cd55f8
cutewindy <wengwendi@gmail.com>
2015-05-11 16:35:28 +0800

    First solution

--- Find_Minimun_in_Rotated_Sorted_Array.py
+++ Find_Minimun_in_Rotated_Sorted_Array.py
@@ -7,10 +7,53 @@
 # You may assume no duplicate exists in the array.
 
 def findMin(nums):
-    minnum = nums[0]
-    for num in nums:
-        minnum = min(num, minnum)
+    minnum = min(nums)
     return minnum
 
 print findMin([0, 1, 2, 4, 5, 6, 7])
+
+
+def findMinI(nums):
+    star, end = 0, len(nums) - 1
+    min = nums[0]
+    while star <= end:
+        mid = (star + end) / 2
+        if nums[mid] >= min:
+            star = mid + 1
+        else:
+            min = nums[mid]
+            end = mid
+
+    return min
+
+print findMinI([0, 1, 2, 4, 5, 6, 7]) 
+print findMinI([4, 5, 6, 7, 0, 1, 2])   
+print findMinI([1])
+print findMinI([2, 1]) 
+
+
+
+def findMinII(nums):
+    if nums[0] <= nums[-1]:
+        return nums[0]
+    mid = len(nums) / 2
+    if nums[mid] < nums[mid-1]:
+        return nums[mid]
+    if nums[mid] > nums[-1]:
+        return findMinII(nums[mid+1:])
+    else:
+        return findMinII(nums[:mid])
+    
         
+print findMinII([0, 1, 2, 4, 5, 6, 7]) 
+print findMinII([4, 5, 6, 7, 0, 1, 2])   
+print findMinII([1])
+print findMinII([2, 1]) 
+print findMinII([1, 2])
+    
+    
+    
+    
+    
+
+  
